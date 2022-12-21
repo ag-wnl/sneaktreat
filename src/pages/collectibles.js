ccfd9a0c8329c46edf622e5ac237edd1c1509b6e
Agnideep <agnideep22@gmail.com>
2022-12-21 14:59:30 +0530

    release 1.0.1

--- src/pages/collectibles.js
+++ src/pages/collectibles.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import Header from '../components/Header'
+import Product from '../components/Product';
 
-function collectibles() {
+export async function getServerSideProps(context) {
+  const products = await fetch("https://api.npoint.io/06c058f2d597ee5251d3").then(
+    (res) => res.json()
+  );
+
+  return { props :{
+    products,
+    
+  },
+};
+}
+
+
+function collectibles({ products }) {
+
+  const filteredSneakCollectible = products.filter(product => product.category === "collectibles");
+
   return (
     <div>
 
@@ -11,6 +28,20 @@ function collectibles() {
             <h1 className='tracking-widest ml-8 p-8 text-xl text-gray-700'>COLLECTIBLES</h1>
 
             <h2 className='tracking-wider ml-14 mr-2 p-1 text-xs text-gray-800 mx-auto font-medium'>A collection is incomplete without items that you cherish, items which carry a story with them.</h2>
+        </div>
+
+        <div className='grid grid-flow-row-dense xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 mx-auto'>
+            {filteredSneakCollectible.map(({id, title, price, description, category, image}) => (
+                <Product 
+                    key={id}
+                    id={id}
+                    title={title}
+                    price={price}
+                    description={description}
+                    category={category}
+                    image={image}
+                />
+            ))}
         </div>
 
     </div>
