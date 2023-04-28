402fe1e30abc8930cc47d6b66f48384ac91db5bf
Agnideep <agnideep22@gmail.com>
2023-04-28 11:55:01 +0530

    Whee, good night.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Link from 'next/link'
+import { useSession, signIn, signOut } from "next-auth/react"
+
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
+import { useState } from 'react';
 
 function Banner() {
+
+    const router = useRouter();
+
   return (
     <div className='relative'>
         
@@ -15,16 +25,21 @@ function Banner() {
             interval={5000}
             
         >
-            <div>
+            <div onClick={() => router.push("/raredrops")} className="link">
                 {/* loading='lazy'  */}
-                <img src='https://i.imgur.com/YGmk2qL.png' alt='banner' />
-            
-            </div>
-                <img src='https://i.imgur.com/Vev4Mpo.png' alt='banner' />
-            
-            <div>
+                {/* https://i.imgur.com/YGmk2qL.png */}
 
-                <img src='https://i.imgur.com/AnkaPEf.png' alt='banner' />
+                
+                <img src='https://i.imgur.com/ToYGZuf.png' alt='banner' />              
+            </div>
+            
+            <div onClick={() => router.push("/apparel")} className="link">
+                <img src='https://i.imgur.com/Fk7V2wP.jpg' alt='banner' />
+            </div>
+            
+            <div onClick={() => router.push("/raredrops")} className="link">
+
+                <img src='https://i.imgur.com/RStHaxQ.jpg' alt='banner' />
 
             </div>
            
